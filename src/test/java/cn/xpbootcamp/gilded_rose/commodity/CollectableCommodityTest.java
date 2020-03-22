79be4bca254fcb74baa7621e884c8d66f6463445
junsong.lai <junsong.lai@thoughtworks.com>
2020-03-22 18:32:26 +0800

    add update collectable commodity stat test

--- src/test/java/cn/xpbootcamp/gilded_rose/commodity/CollectableCommodityTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/commodity/CollectableCommodityTest.java
@@ -3,6 +3,7 @@ package cn.xpbootcamp.gilded_rose.commodity;
 import cn.xpbootcamp.gilded_rose.commodity.exception.InvalidCommodityQualityError;
 import org.junit.jupiter.api.Test;
 
+import static cn.xpbootcamp.gilded_rose.commodity.CommodityFactory.QUALITY_MAX;
 import static cn.xpbootcamp.gilded_rose.commodity.CommodityType.COLLECTABLE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,5 +25,19 @@ public class CollectableCommodityTest {
     public void should_create_collectable_commodity_failed() {
         assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createCollectableCommodity(COMMODITY_NAME, 2, -1));
         assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createCollectableCommodity(COMMODITY_NAME, 2, 52));
+    }
+
+    @Test
+    public void should_update_collectable_commodity_stat_correct() {
+        Commodity commodity = CommodityFactory.createCollectableCommodity(COMMODITY_NAME, 2, 30);
+
+        commodity.updateStat();
+        assertEquals(commodity.getSellIn(), 1);
+        assertEquals(commodity.getQuality(), 31);
+
+        for (int i = 0; i < 50; i++) {
+            commodity.updateStat();
+        }
+        assertEquals(commodity.getQuality(), QUALITY_MAX);
     }
 }
