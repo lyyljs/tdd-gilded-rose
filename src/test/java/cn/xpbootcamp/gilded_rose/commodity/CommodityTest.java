4d8d27ad5d8aab301843777a0bf789d677f06e30
junsong.lai <junsong.lai@thoughtworks.com>
2020-03-22 17:30:21 +0800

    add create general commodity tests

---
+++ src/test/java/cn/xpbootcamp/gilded_rose/commodity/CommodityTest.java
@@ -0,0 +1,28 @@
+package cn.xpbootcamp.gilded_rose.commodity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CommodityTest {
+
+    private static final String DEFAULT_NAME = "G1";
+
+    @Test
+    public void should_create_general_commodity_success() {
+        int sellIn = 1;
+        int quality = 10;
+        Commodity commodity = CommodityFactory.createGeneralCommodity(DEFAULT_NAME, 1, 10);
+        assertEquals(commodity.getName(), DEFAULT_NAME);
+        assertEquals(commodity.getType(), CommodityType.GENERAL);
+        assertEquals(commodity.getSellIn(), sellIn);
+        assertEquals(commodity.getQuality(), quality);
+    }
+
+    @Test
+    public void should_create_general_commodity_failed() {
+        assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createGeneralCommodity(DEFAULT_NAME, 1, -1));
+        assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createGeneralCommodity(DEFAULT_NAME, 1, 52));
+    }
+}
