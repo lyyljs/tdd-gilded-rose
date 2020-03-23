1b88307745ab1e0c4415c18d45a246bdfd5e3e97
junsong.lai <junsong.lai@thoughtworks.com>
2020-03-23 10:05:39 +0800

    add create immortal commodity test

---
+++ src/test/java/cn/xpbootcamp/gilded_rose/commodity/ImmortalCommodityTest.java
@@ -0,0 +1,26 @@
+package cn.xpbootcamp.gilded_rose.commodity;
+
+import cn.xpbootcamp.gilded_rose.commodity.exception.InvalidCommodityQualityError;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ImmortalCommodityTest {
+    private static final String COMMODITY_NAME = "Sulfuras";
+
+    @Test
+    public void should_create_immortal_commodity_success() {
+        Commodity commodity = CommodityFactory.createImmortalCommodity(COMMODITY_NAME, 2, 10);
+        assertEquals(commodity.getName(), COMMODITY_NAME);
+        assertEquals(commodity.getSellIn(), 2);
+        assertEquals(commodity.getQuality(), 10);
+        assertEquals(commodity.getType(), IMMORTAL);
+    }
+
+    @Test
+    public void should_create_immortal_commodity_failed() {
+        assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createImmortalCommodity(COMMODITY_NAME, 2, -1));
+        assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createImmortalCommodity(COMMODITY_NAME, 2, 52));
+    }
+}
