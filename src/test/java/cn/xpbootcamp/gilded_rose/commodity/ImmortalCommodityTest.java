05ef265f982b19054287a85b2199cfe08b4839b8
junsong.lai <junsong.lai@thoughtworks.com>
2020-03-23 10:37:38 +0800

    add update immortal commodity stat test

--- src/test/java/cn/xpbootcamp/gilded_rose/commodity/ImmortalCommodityTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/commodity/ImmortalCommodityTest.java
@@ -3,6 +3,7 @@ package cn.xpbootcamp.gilded_rose.commodity;
 import cn.xpbootcamp.gilded_rose.commodity.exception.InvalidCommodityQualityError;
 import org.junit.jupiter.api.Test;
 
+import static cn.xpbootcamp.gilded_rose.commodity.CommodityFactory.QUALITY_MIN;
 import static cn.xpbootcamp.gilded_rose.commodity.CommodityType.IMMORTAL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -23,5 +24,20 @@ public class ImmortalCommodityTest {
     public void should_create_immortal_commodity_failed() {
         assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createImmortalCommodity(COMMODITY_NAME, 2, -1));
         assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createImmortalCommodity(COMMODITY_NAME, 2, 52));
+    }
+
+    @Test
+    public void should_update_immortal_commodity_stat_correct() {
+        int quality = 10;
+        Commodity commodity = CommodityFactory.createImmortalCommodity(COMMODITY_NAME, 1, quality);
+
+        commodity.updateStat();
+        assertEquals(commodity.getSellIn(), 0);
+        assertEquals(commodity.getQuality(), quality);
+
+        for (int i = 0; i < 10; i++) {
+            commodity.updateStat();
+            assertEquals(commodity.getQuality(), quality);
+        }
     }
 }
