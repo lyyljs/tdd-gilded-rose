eb60a16728668b38a27d4f18b0ca9d72ecc6dee8
junsong.lai <junsong.lai@thoughtworks.com>
2020-03-23 11:06:12 +0800

    add update ticket commodity test

--- src/test/java/cn/xpbootcamp/gilded_rose/commodity/TicketCommodityTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/commodity/TicketCommodityTest.java
@@ -3,6 +3,7 @@ package cn.xpbootcamp.gilded_rose.commodity;
 import cn.xpbootcamp.gilded_rose.commodity.exception.InvalidCommodityQualityError;
 import org.junit.jupiter.api.Test;
 
+import static cn.xpbootcamp.gilded_rose.commodity.CommodityFactory.QUALITY_MAX;
 import static cn.xpbootcamp.gilded_rose.commodity.CommodityType.TICKET;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -23,5 +24,34 @@ public class TicketCommodityTest {
     public void should_create_ticket_commodity_failed() {
         assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createTicketCommodity(COMMODITY_NAME, 2, -1));
         assertThrows(InvalidCommodityQualityError.class, () -> CommodityFactory.createTicketCommodity(COMMODITY_NAME, 2, 52));
+    }
+
+    @Test
+    public void should_update_ticket_commodity_stat_correct() {
+        int quality = 30;
+        int sellIn = 11;
+        Commodity commodity = CommodityFactory.createImmortalCommodity(COMMODITY_NAME, sellIn, quality);
+
+        commodity.updateStat();
+        assertEquals(commodity.getSellIn(), 10);
+        assertEquals(commodity.getQuality(), 31);
+
+        commodity.updateStat();
+        assertEquals(commodity.getSellIn(), 9);
+        assertEquals(commodity.getQuality(), 33);
+
+        for (int i = 0; i < 4; i++) {
+            commodity.updateStat();
+        }
+        assertEquals(commodity.getQuality(), 41);
+
+        commodity.updateStat();
+        assertEquals(commodity.getSellIn(), 4);
+        assertEquals(commodity.getQuality(), 44);
+
+        for (int i = 0; i < 5; i++) {
+            commodity.updateStat();
+        }
+        assertEquals(commodity.getQuality(), 0);
     }
 }
